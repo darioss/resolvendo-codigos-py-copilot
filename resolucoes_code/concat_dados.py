ac51e3793086c184c8988a6f1529649454fd6e1c
Dario Silva <dariosouzadasilva@gmail.com>
2025-01-14 23:20:47 -0300

    Update concat_dados.py
    Desafio 1

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,12 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+# Solicita os dados ao usuário
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+
+# Concatena os dados em uma única string
+resultado = dado1 + " " + dado2
+
+# Exibe o resultado
+print("Resultado da concatenação:", resultado)
+
