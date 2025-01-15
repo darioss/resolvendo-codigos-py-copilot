c20d22e782201effd357cff33bf8076dc872dc7c
Dario Silva <dariosouzadasilva@gmail.com>
2025-01-14 23:45:31 -0300

    Atualizar o repet_txt.py
    solução desafio 3

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,9 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+# Solicitar a string e o número inteiro
+texto = input("Digite uma string: ")
+numero = int(input("Digite um número inteiro: "))
+
+# Imprimir a string o número de vezes solicitado, em linhas separadas
+for _ in range(numero):
+    print(texto)
